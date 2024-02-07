a42d6994d3af41a17f31d242ea8a961226a63b8b
mfatihrabbani <mfatihrabbani@gmail.com>
2024-02-07 23:03:46 +0700

    feat : making script for content youtube

---
+++ warp-impl.lua
@@ -0,0 +1,38 @@
+function warp(world, id)
+    local countWarp = 0
+    if id == nil or id == "" then
+        while getWorld().name:upper() ~= world:upper() do
+            getBot():warp(world:upper())
+            sleep(5000)
+            countWarp = countWarp + 1
+            if countWarp == 5 then
+                countWarp = 0
+                getBot():disconnect()
+                sleep(10000)
+                while getBot().status ~= BotStatus.online do
+                    getBot():connect()
+                    sleep(5000)
+                end
+            end
+        end
+    else
+        while getWorld().name:upper() ~= world:upper() 
+        or getWorld():getTile(math.floor(getWorld():getLocal().posx/32), math.floor(getWorld():getLocal().posy/32)).fg == 6 do
+            getBot():warp(world:upper(), id:upper())
+            sleep(5000)
+            if countWarp == 5 then
+                countWarp = 0
+                getBot():disconnect()
+                sleep(10000)
+                while getBot().status ~= BotStatus.online do
+                    getBot():connect()
+                    sleep(5000)
+                end
+            end
+        end
+    end
+end
+
+warp("INDONESIA")
+
+warp("WORLDBREAK", "IDWORLDBREAK")
